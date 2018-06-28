193ffee7fa37d804e1eeae976da6ea9b1f263555
Jose Briz <brizjosem@gmail.com>
2018-06-28 06:03:03 -0500

    one more algo practice

--- Algorithms/algo_algos/41_probabilities.js
+++ Algorithms/algo_algos/41_probabilities.js
@@ -1,5 +1,14 @@
 function weightedRandom(){
     var weights = [0.1, 0.15, 0.2, 0.25, 0.3];
     var list = ['volcano', 'tsunami', 'earthquake', 'blizzard', 'meteor'];
-    
+    var max = 0;
+    var count = 0;
+    for(var i = 0; i < weights.length; i++){
+        if(weights[i] > max){
+            max = weights[i];
+            count++;
+        }
+            }
+    console.log(list[count-1]);
 }
+weightedRandom();
