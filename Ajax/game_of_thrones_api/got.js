86f297e3d40c9e22e51e9b27363958a6d5169e1c
Jose Briz <brizjosem@gmail.com>
2018-06-30 09:03:06 -0500

    game of thrones ajax assignment done

--- Ajax/game_of_thrones_api/got.js
+++ Ajax/game_of_thrones_api/got.js
@@ -1,11 +1,27 @@
 $(document).ready(function(){
 
-        $('.container').on('click','img',function(){
-            var which = $(this).attr('alt');
-            $.get('https://anapioficeandfire.com/api/houses/', function(response){
-                console.log(response);
+        $('h1').click(function(){
+            $('.details').html("");
+        });
 
+        $('.container').on('click','img',function(){
+            $('.details').html("");
+            var which = $(this).attr('data-id');
+            $.get('https://anapioficeandfire.com/api/houses/'+which, function(response){
+                // console.log(response);
+                var html_str = `
+                                <h2>${response.name}</h2>
+                                <h3>"${response.words}"</h3>
+                                <p>${response.titles}</p>
+                `;
+                $('.details').append(html_str)
             }, 'json');
         });
 
 })
+
+// https://anapioficeandfire.com/api/houses?page=1&pageSize=50
+// https://anapioficeandfire.com/api/houses?name=House%20Stark%20of%20Winterfell  or 
+// https://anapioficeandfire.com/api/houses?name=House%20Baratheon%20of%20Storm%27s%20End  or 17
+// https://anapioficeandfire.com/api/houses?name=House%20Lannister%20of%20Casterly%20Rock or 229
+// https://anapioficeandfire.com/api/houses?name=House%20House%20Targaryen%20of%20King%27s%20Landing or 378
