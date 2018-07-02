1de361dc628343338560ce1108c6ee238d98c534
Jose Briz <brizjosem@gmail.com>
2018-07-01 23:10:02 -0500

    python math assignment

---
+++ Python/math_assignment.py
@@ -0,0 +1,18 @@
+for i in range (0,11):
+    if i % 2 == 0:
+        print i
+
+
+for i in range (5,51):
+    if i % 5 == 0:
+        print i
+
+
+a = [1, 2, 5, 10, 255, 3]
+num = sum(a)
+print num    
+
+
+a = [1, 2, 5, 10, 255, 3]
+num = sum(a)
+print num/len(a)
