cffab4894a7ff04b9992890c32161687796fb34b
Jose Briz <brizjosem@gmail.com>
2018-07-03 09:15:22 -0500

    checkerboard assign correction

--- Python/checkerboard_assignment.py
+++ Python/checkerboard_assignment.py
@@ -1,11 +1,8 @@
 pos = "* "
 neg = " *"
-br = "\n"
 
-for i in range (0,9):
-    if i % 8 == 0:
-        output = br
-    elif i % 2 == 0:
+for i in range (0,8):
+    if i % 2 == 0:
         output = neg * 4
     else: 
         output = pos * 4
--- Python/enumerate.py
+++ Python/enumerate.py
@@ -2,11 +2,3 @@ table = [50,100,150,200,250]
 
 num_table = enumerate(table,1)
 print list(num_table)
-
-for count, value in enumerate(table):
-    for i in table:
-        
-
-
-
-
