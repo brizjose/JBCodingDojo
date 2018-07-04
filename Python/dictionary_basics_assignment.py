1f733046869ecaf227c95ba6770d9285d618e840
Jose Briz <brizjosem@gmail.com>
2018-07-04 04:39:42 -0500

    python dictionary basics assignment

---
+++ Python/dictionary_basics_assignment.py
@@ -0,0 +1,10 @@
+me ={
+    "name":"Jose",
+    "age":"43",
+    "country of birth":"US",
+    "favorite language":"python"
+}
+
+for key,data in me.iteritems():
+    print "My {} is {}".format(key,data)
+
