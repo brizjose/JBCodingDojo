580c812798f64b685a43e0c3057b44f6388f4d90
Jose Briz <brizjosem@gmail.com>
2018-07-08 22:33:58 -0500

    flask with session

--- python_stack/flask_fundamentals/disappearing_ninja_assignment/server.py
+++ python_stack/flask_fundamentals/disappearing_ninja_assignment/server.py
@@ -1,10 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect
 app = Flask(__name__)
+app.secret_key = "creepyninja"
+
 @app.route('/')
 def default():
     return render_template("default.html")
 @app.route('/ninja')
 def ninjas():
+    # if "ninjas" not in session:
+    #     session["ninjas"].append(
+    #         {"name":"Leonardo", "color":"blue", "type":"turtle"},
+    #         {"name":"Michelangelo", "color":"orange", "type":"turtle"},
+    #         {"name":"Raphael", "color":"red", "type":"turtle"},
+    #         {"name":"Donatello", "color":"purple", "type":"turtle"},
+    #         {"name":"April", "color":"none", "type":"human"}
+    #     )
+    # print ninjas
+    # return redirect('/')
     return render_template("ninja.html")
 
 app.run(debug=True)
