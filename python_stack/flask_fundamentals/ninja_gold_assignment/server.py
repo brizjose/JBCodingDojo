6978ea984a21efa80e570fc4f93b9587404d211e
Jose Briz <brizjosem@gmail.com>
2018-07-10 11:34:49 -0500

    ninja gold assignment

--- python_stack/flask_fundamentals/ninja_gold_assignment/server.py
+++ python_stack/flask_fundamentals/ninja_gold_assignment/server.py
@@ -5,27 +5,31 @@ app.secret_key = "earnmoneyninja"
 
 @app.route("/")
 def index():
+    session['earnings'] = 0
+    session['activity'] = []
+    session['messages'] = []
     return render_template("index.html")
 
 @app.route("/earn", methods=['POST'])
 def earn():
-    session['earnings'] = 0
-    session['activity'] = []
+    print request.form['activity']
     if request.form['activity'] == "Farm":
-        earns = random.randint(10,21)
+        earns = random.randint(10,20)
         session['activity'].append("Farm")
     elif request.form['activity'] == "Cave":
-        earns = random.randint(5,11)
+        earns = random.randint(5,10)
         session['activity'].append("Cave")
     elif request.form['activity'] == "House":
-        earns = random.randint(2,6)
+        earns = random.randint(2,5)
         session['activity'].append("House")
     elif request.form['activity'] == "Casino":
-        earns = random.randint(-50,51)
+        earns = random.randint(-50,50)
         session['activity'].append("Casino")
     else:
         earns = 0
     session['earnings'] += earns
-    return redirect('/')    
+    message = "Earned {} golds from the {}!".format(earns,request.form['activity'])
+    session['messages'].append(message)
+    return render_template("index.html", earnings=session['earnings'], messages=session['messages'], earns=earns)    
 
 app.run(debug=True)
