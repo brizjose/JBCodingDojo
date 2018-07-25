effc0f53bbc5f625ef69a4bb8c6ce0fd629d01ae
Jose Briz <brizjosem@gmail.com>
2018-07-24 21:23:47 -0500

    log in and registration assignment

--- python_stack/flask_MySQL/login_registration_assignment/server.py
+++ python_stack/flask_MySQL/login_registration_assignment/server.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, redirect, session, flash
 from mysqlconnection import MySQLConnector
+from flask.ext.bcrypt import Bcrypt
 app = Flask(__name__)
+bcrypt = Bcrypt(app)
 app.secret_key = "regisvalid1234"
 mysql = MySQLConnector(app, 'users')
 
@@ -13,16 +15,42 @@ def index():
 
 #REGISTRATION
 @app.route('/register', methods=['POST'])
-def register():
-    return "got to register"
+def register():    
     #receive data
+    print request.form  
         #validate data
+    valid = True
+    if len(request.form['fname']) < 1:
+        valid = False
+        flash("Please enter a full first name")
+    if len(request.form['lname']) < 1:
+        valid = False
+        flash("Please enter a full last name")
+    if len(request.form['email']) < 5:
+        valid = False
+        flash("Please enter a full email")
+    if len(request.form['pword']) < 8:
+        valid = False        
+        flash("Please make password at least 8 characters long")
+    if valid != True:
+        return redirect ('/')
             #valid
+    query = "INSERT INTO `users`.`users` (`first_name`, `last_name`, `email`, `password`, `created_at`, `updated_at`) VALUES (:fname, :lname, :email, :pword, NOW(), NOW());"
+    data = {
+        'fname':request.form['fname'],
+        'lname':request.form['lname'],
+        'email':request.form['email'],
+        'pword':bcrypt.generate_password_hash(request.form['pword'])
+    }   
+    mysql.query_db = (query,data)
+    flash("Successfully registered!")
+    return redirect('/')
                 #store it, send it to database
                     #flash a success message
             #not valid
                 #flash error message
             #redirect to index
+    return "got to register"
 
 #LOGIN
 @app.route('/login', methods=['POST'])
