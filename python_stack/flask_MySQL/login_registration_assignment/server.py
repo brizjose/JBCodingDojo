aa53b2358333e07d01dbe4214ab3dc05c9ad0bee
Jose Briz <brizjosem@gmail.com>
2018-07-24 22:43:01 -0500

    login and registration assignment

--- python_stack/flask_MySQL/login_registration_assignment/server.py
+++ python_stack/flask_MySQL/login_registration_assignment/server.py
@@ -1,12 +1,16 @@
 from flask import Flask, render_template, request, redirect, session, flash
 from mysqlconnection import MySQLConnector
-from flask.ext.bcrypt import Bcrypt
-app = Flask(__name__)
-bcrypt = Bcrypt(app)
-app.secret_key = "regisvalid1234"
-mysql = MySQLConnector(app, 'users')
+import md5
+import re
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
 
-#print mysql.query_db("SELECT * FROM users")
+app = Flask(__name__)
+
+app.secret_key = "regisvalid1234"
+
+mysql = MySQLConnector(app,'login_registration')
+
+print mysql.query_db("SELECT * FROM users")
 
 @app.route('/')
 def index():
@@ -29,20 +33,23 @@ def register():
     if len(request.form['email']) < 5:
         valid = False
         flash("Please enter a full email")
+    if not EMAIL_REGEX.match(request.form['email']):
+        valid = False
+        flash("Invalid email address!")
     if len(request.form['pword']) < 8:
         valid = False        
         flash("Please make password at least 8 characters long")
     if valid != True:
         return redirect ('/')
             #valid
-    query = "INSERT INTO `users`.`users` (`first_name`, `last_name`, `email`, `password`, `created_at`, `updated_at`) VALUES (:fname, :lname, :email, :pword, NOW(), NOW());"
+    query = "INSERT INTO `users` (`first_name`, `last_name`, `email`, `password`, `created_at`, `updated_at`) VALUES (:fname, :lname, :email, :pword, now(), now());"
     data = {
         'fname':request.form['fname'],
         'lname':request.form['lname'],
         'email':request.form['email'],
-        'pword':bcrypt.generate_password_hash(request.form['pword'])
+        'pword':md5.new(request.form['pword']).hexdigest()
     }   
-    mysql.query_db = (query,data)
+    mysql.query_db(query,data)
     flash("Successfully registered!")
     return redirect('/')
                 #store it, send it to database
@@ -50,16 +57,44 @@ def register():
             #not valid
                 #flash error message
             #redirect to index
-    return "got to register"
 
 #LOGIN
 @app.route('/login', methods=['POST'])
 def login():
-    return "got to login"
     #receive data
         #validate data
+    valid = True
+    if len(request.form['email']) < 5:
+        valid = False
+        flash("Please enter a full email")
+    if not EMAIL_REGEX.match(request.form['email']):
+        valid = False
+        flash("Invalid email address!")
+    if len(request.form['pword']) < 8:
+        valid = False        
+        flash("Please make password at least 8 characters long")
+    if valid != True:
+        return redirect ('/')        
             #valid
+    else:
+        query = "SELECT * FROM users WHERE email = :email"
+        data = {
+            'email':request.form['email']
+        }
+        users = mysql.query_db(query,data)
+        if len(users) > 0:
+            user = users[0]
+            password = md5.new(request.form['pword']).hexdigest()
+            if password == users[0]['password']:
+                session['user_id'] = user['id']
+                flash("Succesful login, user id:{}".format(session['user_id']))
+            else:
+                flash("Please check credentials") 
+        else:
+            flash("Email does not exist")
+    return redirect('/')
                 #check database for user
+
                     #if user
                         #check password
                             #if password match
