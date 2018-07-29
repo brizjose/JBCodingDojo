f4753625933b940038f89bda0b5171145f51767e
Jose Briz <brizjosem@gmail.com>
2018-07-29 08:31:11 -0500

    python OOP studies

--- python_stack/python_OOP/9_super.py
+++ python_stack/python_OOP/9_super.py
@@ -6,22 +6,22 @@
 
 #example, suppose there is a Human object and you want to modify at the sub-class level...
 
-from human import Human
-class Wizard(Human):
-    def __init__(self):
-        super(Wizard, self).__init__()   # use super to call the Human __init__ method
-        self.intelligence = 10           # every wizard starts off with 10 intelligence
-    def heal(self):
-        self.health += 10
-class Ninja(Human):
-    def __init__(self):
-        super(Ninja, self).__init__()    # use super to call the Human __init__ method
-        self.stealth = 10                # every Ninja starts off with 10 stealth
-    def steal(self):
-        self.stealth += 5
-class Samurai(Human):
-    def __init__(self):
-        super(Samurai, self).__init__()  # use super to call the Human __init__ method
-        self.strength = 10               # every Samurai starts off with 10 strength
-    def sacrifice(self):
-        self.health -= 5
+    # from human import Human
+    # class Wizard(Human):
+    #     def __init__(self):
+    #         super(Wizard, self).__init__()   # use super to call the Human __init__ method
+    #         self.intelligence = 10           # every wizard starts off with 10 intelligence
+    #     def heal(self):
+    #         self.health += 10
+    # class Ninja(Human):
+    #     def __init__(self):
+    #         super(Ninja, self).__init__()    # use super to call the Human __init__ method
+    #         self.stealth = 10                # every Ninja starts off with 10 stealth
+    #     def steal(self):
+    #         self.stealth += 5
+    # class Samurai(Human):
+    #     def __init__(self):
+    #         super(Samurai, self).__init__()  # use super to call the Human __init__ method
+    #         self.strength = 10               # every Samurai starts off with 10 strength
+    #     def sacrifice(self):
+    #         self.health -= 5
