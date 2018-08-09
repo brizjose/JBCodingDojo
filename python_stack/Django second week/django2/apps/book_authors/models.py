33990c868e6a9cc54e4b9bfc7853457cc38140a1
Jose Briz <brizjosem@gmail.com>
2018-08-09 04:31:01 -0500

    django book_authors assignment

--- python_stack/Django second week/django2/apps/book_authors/models.py
+++ python_stack/Django second week/django2/apps/book_authors/models.py
@@ -9,6 +9,7 @@ class Author(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     email = models.CharField(max_length=255)
+    notes = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     def __repr__(self):
@@ -17,15 +18,10 @@ class Author(models.Model):
 class Book(models.Model):
     name = models.CharField(max_length=255)
     desc = models.TextField(max_length=1000)
-    authors = models.ManyToManyField(Author, related_name="authors")
+    authors = models.ManyToManyField(Author, related_name="books")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     def __repr__(self):
         return "<{}>".format(self.name)
 
 
-#example of python shell commands to assign authors to books:
-#php_authors = Author.objects.filter(email__contains="php")
-#book_php = Book.objects.filter(name__contains="PHP")
-#book_php.authors = php_authors
-#print book_php.authors
