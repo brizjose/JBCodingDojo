1c48c6868a25aa5a6fe5393492dad71e61e7395d
Jose Briz <brizjosem@gmail.com>
2018-08-08 20:49:57 -0500

    django orm lectures

--- python_stack/Django second week/django2/django2/urls.py
+++ python_stack/Django second week/django2/django2/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^user_login/', include('apps.user_login.urls')),
+    url(r'^dojo_ninjas/', include('apps.dojo_ninjas.urls')),
     url(r'^admin/', admin.site.urls),
 ]
