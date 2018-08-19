ff83674ae1e41c6815f504cca6d4952df21de2c3
Jose Briz <brizjosem@gmail.com>
2018-08-19 12:04:51 -0500

    done some folder modifications

---
+++ python_stack/Django with Ajax/basic_ajax_form.js
@@ -0,0 +1,9 @@
+/* in the html file, inside <spript> tags </spript> or in a linked .js file, this code captures form data and does the following: */
+
+
+$.ajax({
+    url: /* Where should this go? */,
+    method: /* Which HTTP verb? */,
+    data: /* Any data to send along? */,
+    success: /* What code should we run when the server responds? */
+   })
---
+++ python_stack/myEnvironments/Python2Django/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\josebr~1\desktop\coding~1\codes\jbcodi~1\python~1\myenvi~1\python2django\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
