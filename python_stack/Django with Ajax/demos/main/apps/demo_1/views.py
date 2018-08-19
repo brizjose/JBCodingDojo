bd19ee9f7bee3d62715c46f66ce452aeae9c1c71
Jose Briz <brizjosem@gmail.com>
2018-08-19 13:44:37 -0500

    working on first django ajax example

--- python_stack/Django with Ajax/demos/main/apps/demo_1/views.py
+++ python_stack/Django with Ajax/demos/main/apps/demo_1/views.py
@@ -6,4 +6,4 @@ from django.shortcuts import render, HttpResponse, redirect
 # Create your views here.
 
 def index(request):
-    return HttpResponse("welcome to the first django ajax project")
+    return render(request, 'demo_1/index.html')
