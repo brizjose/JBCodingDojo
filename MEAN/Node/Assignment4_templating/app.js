339ba0a196e0464e95eab83e3d126fead92b5d9a
Jose Briz <brizjosem@gmail.com>
2018-12-09 21:47:51 -0600

    ejs templating exercise

--- MEAN/Node/Assignment4_templating/app.js
+++ MEAN/Node/Assignment4_templating/app.js
@@ -1,12 +1,15 @@
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
-const path = require('path');
 
 const rootDir = require('./utils/path');
 const userRoutes = require('./routes/user.routes');
 
 const app = express();
 
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(rootDir, 'public')));
@@ -14,7 +17,7 @@ app.use(express.static(path.join(rootDir, 'public')));
 app.use(userRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(rootDir, 'views', 'not-found.html'));
+    res.redirect(404, 'not-found');
 })
 
 app.listen(3000);
--- MEAN/Node/Assignment4_templating/routes/user.routes.js
+++ MEAN/Node/Assignment4_templating/routes/user.routes.js
@@ -4,14 +4,21 @@ const rootDir = require('../utils/path');
 
 const router = express.Router();
 
+const users = [];
+
 router.get('/', (req, res, next) => {
     console.log('hit the root route');
-    res.sendFile(path.join(rootDir, 'views', 'index.html'));
+    res.render('index', {pageTitle: 'User Form'});
+})
+
+router.post('/new', (req, res, next) => {
+    users.push(req.body);
+    res.redirect('/users');
 })
 
 router.get('/users', (req, res, next) => {
     console.log('hit the users route');
-    res.sendFile(path.join(rootDir, 'views', 'users.html'));
+    res.render('users', {pageTitle: 'Users', users: users});
 })
 
 module.exports = router;
